884646892c3a6e97e86b557353f4576689907d00
kitoketa <rooney2406@gmail.com>
2020-04-18 12:02:19 +0900

    implement event new compornent

--- src/index.js
+++ src/index.js
@@ -3,17 +3,24 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import './index.css';
 import reducer from './reducers'
-import EventIndex from './compornents/events_index';
+import EventsIndex from './compornents/events_index';
+import EventsNew from './compornents/events_new';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
     <Provider store={store}>
-        <EventIndex />
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/events/new" component={EventsNew} />
+                <Route exact path="/" component={EventsIndex} />
+            </Switch>
+        </BrowserRouter>
     </Provider>,
         document.getElementById('root')
 );
